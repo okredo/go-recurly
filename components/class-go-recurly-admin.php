276f4e6aa068ac51d4e42915eafeb8b84dd110bb
Will Luo <will.luo@gigaom.com>
2014-10-22 12:37:01 -0700

    reverted the revert of 6966e97608dae2e16b3c28f3fe4f48a07de26d01: it turns out it was caused by missing allowed html to the wp_kses() call

--- components/class-go-recurly-admin.php
+++ components/class-go-recurly-admin.php
@@ -221,7 +221,7 @@ class GO_Recurly_Admin
 	 	?>
 	 	<tr class="form-field">
 			<th><?php echo esc_html( $key ); ?></th>
-			<td><code><?php echo ( 'account_code' == $key ? wp_kses( $value, $allowed_html ) : esc_html( $value ) ); ?></code></td>
+			<td><code><?php echo ( 'account_code' == $key ) ? wp_kses( $value, $allowed_html ) : esc_html( $value ); ?></code></td>
 		</tr>
 		<?php
 	}//end show_user_profile_row
